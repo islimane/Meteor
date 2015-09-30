0d9b53db14351b8b0088cba4bf8a57f70b919c2d
ismael slimane zubillaga <islimane@zeta40.aulas.gsyc.es>
2015-09-30 19:35:57 +0200

    Added Initial Form to create an event

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -1,9 +1,19 @@
 UsersList = new Mongo.Collection('users');
+EventFields = new Mongo.Collection('fileds');
+
 if(Meteor.isClient){
     // this code only runs on the client
     Template.doodleBoard.helpers({
       'user': function(){
         return UsersList.find({}, {sort: {score: -1, name: 1} });
+      },
+      'createBoard': function(){
+        var dates = EventFields.find().fetch()[0].dates;
+        var datesText = "";
+        for (i in dates) {
+          datesText += "<th>" + dates[i] + " - 10:00-12:00</th>"
+        }
+        return datesText;
       }
     });
 
@@ -18,6 +28,32 @@ if(Meteor.isClient){
       }
     });
 
+    Template.main.helpers({
+      'createdPoll': function(){
+        // If this sentence return something then true
+        return EventFields.find().fetch();
+      }
+    });
+
+    Template.main.events({
+      'submit form': function(event){
+        event.preventDefault();
+        var eventName = event.target.eventName.value;
+        var userName = event.target.userName.value;
+        var dates = event.target.dates.value.split(",");
+        EventFields.insert({
+            title: eventName,
+            name: userName,
+            dates: dates
+        });
+      }
+    });
+
+    Template.initForm.rendered=function() {
+        $('#my-datepicker').datepicker({
+          multidate: true
+        });
+    }
 }
 if(Meteor.isServer){
     // this code only runs on the client
