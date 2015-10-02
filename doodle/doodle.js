c2ff6cce0a1b5cb35f3ed9dab5e4e064f6f2f771
Ismael Slimane <isma.bled777@gmail.com>
2015-10-02 12:36:18 +0200

    Added some updates

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -1,19 +1,42 @@
 UsersList = new Mongo.Collection('users');
 EventFields = new Mongo.Collection('fileds');
 
+var myTemplate = "ada";
+
+// this function return true if there is
+// an empty string in the array, otherwise
+// returns false
+var thereIsAnEmptyString = function(strArr){
+  for (i in strArr)
+    if(strArr[i] === "")
+      return true;
+  return false;
+};
+
+// This function proccess the initial form's information
+var procForm = function(eventName, userName, dates){
+  var instants = [];
+  dates.forEach(function(element, index, array){
+    instants.push({
+      date: element,
+      time: ""
+    });
+  });
+  EventFields.insert({
+    title: eventName,
+    name: userName,
+    dates: instants
+  });
+};
+
 if(Meteor.isClient){
     // this code only runs on the client
     Template.doodleBoard.helpers({
       'user': function(){
         return UsersList.find({}, {sort: {score: -1, name: 1} });
       },
-      'createBoard': function(){
-        var dates = EventFields.find().fetch()[0].dates;
-        var datesText = "";
-        for (i in dates) {
-          datesText += "<th>" + dates[i] + " - 10:00-12:00</th>"
-        }
-        return datesText;
+      'date': function(){
+        return EventFields.find().fetch()[0].dates;
       }
     });
 
@@ -30,27 +53,53 @@ if(Meteor.isClient){
 
     Template.main.helpers({
       'createdPoll': function(){
-        // If this sentence return something then true
+        // Return false if the collection is not ready
+        if(EventFields.find().fetch().length>0)
+          if(EventFields.find().fetch()[0].dates.hours==='undefined')
+            return false;
         return EventFields.find().fetch();
       }
     });
 
-    Template.main.events({
+    Template.initForm.helpers({
+      'schedule': function(event){
+        event.preventDefault();
+        var eventName = event.target.eventName.value;
+        var userName = event.target.userName.value;
+        var dates = event.target.dates.value.split(",");
+        var strArr = [eventName, userName, event.target.dates.value];
+        if(!thereIsAnEmptyString(strArr)){
+          procForm(eventName, userName, dates);
+        }else{
+          alert("There is some empty field!");
+        }
+      }
+    });
+
+    Template.initForm.events({
       'submit form': function(event){
         event.preventDefault();
         var eventName = event.target.eventName.value;
         var userName = event.target.userName.value;
         var dates = event.target.dates.value.split(",");
-        EventFields.insert({
-            title: eventName,
-            name: userName,
-            dates: dates
-        });
-      }
+        var strArr = [eventName, userName, event.target.dates.value];
+        if(!thereIsAnEmptyString(strArr)){
+          procForm(eventName, userName, dates);
+        }else{
+          alert("There is some empty field!");
+        }
+      },
+      'click #dates': function(event){
+        event.preventDefault();
+        var val = $("#my-datepicker").val();
+        console.log("val: '" + val + "'");
+     },
     });
 
     Template.initForm.rendered=function() {
         $('#my-datepicker').datepicker({
+          format: "dd/mm/yyyy",
+          todayHighlight: true,
           multidate: true
         });
     }
