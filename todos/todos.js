623f9ff27beedb3f01751668f4bac070511aa9c9
Ismael Slimane <isma.bled777@gmail.com>
2015-10-02 21:51:56 +0200

    Routing, Part 2

--- todos/todos.js
+++ todos/todos.js
@@ -1,10 +1,46 @@
+/**
+*
+* ROUTERS
+*
+*/
+
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.route('/register');
+Router.route('/login');
+Router.route('/', {
+  name: 'home',
+  template: 'home'
+});
+Router.route('/list/:_id', {
+  name: 'listPage',
+  template: 'listPage',
+  data: function(){
+    var currentList = this.params._id;
+    return Lists.findOne({ _id: currentList });
+  }
+});
+
+
+/**
+*
+* METEOR APP
+*
+*/
+
+// COLLECTIONS
 Todos = new Meteor.Collection('todos');
+Lists = new Meteor.Collection('lists');
+
 
 if(Meteor.isClient){
     // client code goes here
   Template.todos.helpers({
     'todo': function(){
-        return Todos.find({}, {sort: {createdAt: -1}});
+      var currentList = this._id;
+      return Todos.find({listId:currentList}, {sort:{createdAt:-1}});
     }
   });
 
@@ -13,10 +49,12 @@ if(Meteor.isClient){
     'submit form': function(event){
       event.preventDefault();
       var todoName = $('[name="todoName"]').val();
+      var currentList = this._id;
       Todos.insert({
         name: todoName,
         completed: false,
-        createdAt: new Date()
+        createdAt: new Date(),
+        listId: currentList
       });
       // Empty the text field
       $('[name="todoName"]').val('')
@@ -30,7 +68,7 @@ if(Meteor.isClient){
       var documentId = this._id;
       var confirm = window.confirm("Delete this task?");
       if(confirm){
-        Todos.remove({ _id: documentId });
+        Todos.remove({_id:documentId});
       }
     },
     'keyup [name=todoItem]': function(event){
@@ -45,8 +83,7 @@ if(Meteor.isClient){
     'change [type=checkbox]': function(){
       // code goes here
       var documentId = this._id;
-      var isCompleted = this.completed;
-      Todos.update({_id:documentId}, {$set:{completed:(!isCompleted)}});
+      Todos.update({_id:documentId}, {$set:{completed:(!this.completed)}});
     }
   });
 
@@ -63,12 +100,34 @@ if(Meteor.isClient){
   Template.todosCount.helpers({
       // helpers go here
       'totalTodos': function(){
-        return Todos.find().count();
+        var currentList = this._id;
+        return Todos.find({listId:currentList}).count();
       },
       'completedTodos': function(){
-        return Todos.find({completed:true}).count();
+        var currentList = this._id;
+        return Todos.find({listId:currentList, completed:true}).count();
       }
   });
+
+  Template.addList.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var listName = $('[name=listName]').val();
+      Lists.insert({
+        name: listName
+      }, function(error, results){
+        Router.go('listPage', { _id: results });
+      });
+      $('[name=listName]').val('');
+    }
+  });
+
+  Template.lists.helpers({
+    'list': function(){
+      return Lists.find({}, {sort: {name: 1}});
+    }
+  });
+
 }
 
 if(Meteor.isServer){
