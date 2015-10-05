3fba24c81e00184c3e4602076b94b288b7fd7211
Ismael Slimane <isma.bled777@gmail.com>
2015-10-05 22:08:12 +0200

    updates

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -1,9 +1,5 @@
 UsersList = new Mongo.Collection('users');
 EventsList = new Mongo.Collection('fileds');
-DatesList = new Mongo.Collection('dates');
-dates = [];
-
-var myTemplate = "ada";
 
 // this function return true if there is
 // an empty string in the array, otherwise
@@ -15,24 +11,32 @@ var thereIsAnEmptyString = function(strArr){
   return false;
 };
 
-// This function proccess the initial form's information
-var procForm = function(eventName, userName, dates){
-  var instants = [];
-  dates.forEach(function(element, index, array){
-    instants.push({
-      date: element,
-      time: ""
-    });
-  });
-  EventsList.insert({
-    title: eventName,
-    name: userName,
-    dates: instants
-  });
+var insertDate = function(day, time){
+  var dayHtml = '<td class="day">' + day + '</td>';
+  var timeHtml = '<td class="time">' + time + '</td>';
+  html = '<tr class="date">' + dayHtml + timeHtml + '</tr>'
+  $(html).appendTo('#datesBoard');
 };
 
 if(Meteor.isClient){
   // this code only runs on the client
+  Template.main.helpers({
+    'createdEvent': function(){
+      console.log(EventsList.find().fetch().length>0);
+      if(EventsList.find().fetch())
+        console.log("OK");
+      else
+        console.log("ERROR");
+      return EventsList.find().fetch();
+    }
+  });
+
+  Template.events.helpers({
+    'event': function(){
+      return EventsList.find();
+    }
+  });
+
   Template.doodleBoard.helpers({
     'user': function(){
       return UsersList.find({}, {sort: {score: -1, name: 1} });
@@ -47,25 +51,10 @@ if(Meteor.isClient){
       event.preventDefault();
       var userNameVar = event.target.userName.value;
       console.log(userNameVar);
+      console.log("id: " + this._id);
       UsersList.insert({
           name: userNameVar
       });
-    }
-  });
-
-  Template.main.helpers({
-    'createdPoll': function(){
-      // Return false if the collection is not ready
-      if(EventsList.find().fetch().length>0)
-        if(EventsList.find().fetch()[0].dates.hours==='undefined')
-          return false;
-      return EventsList.find().fetch();
-    }
-  });
-
-  Template.hours.helpers({
-    'dateField': function(){
-      return DatesList.find().fetch()[0].dates;
     }
   });
 
@@ -74,31 +63,47 @@ if(Meteor.isClient){
       event.preventDefault();
       var eventName = event.target.eventName.value;
       var userName = event.target.userName.value;
-      var dates = event.target.dates.value.split(",");
-      var strArr = [eventName, userName, event.target.dates.value];
-      if(!thereIsAnEmptyString(strArr)){
-        procForm(eventName, userName, dates);
-      }else{
+      if(thereIsAnEmptyString([eventName, userName])){
         alert("There is some empty field!");
+        return false;
       }
-    },
-    'click #selectHours': function(event){
-      event.preventDefault();
-      var val = $("#my-datepicker").val();
-      console.log("val: '" + val + "'");
-      dates = $("#my-datepicker").val().split(",");
-      console.log(dates);
-      DatesList.insert({
-          dates: dates
+      var dates = [];
+      $('.day').each(function(index, element){
+        //console.log("element: '" + $(this).text() + "'");
+        dates.push({day: $(this).text()});
       });
+      $('.time').each(function(index, element){
+        //console.log("element: '" + $(this).text() + "'");
+        dates[index].time = $(this).text();
+      });
+      if(dates.length<=0){
+        alert("You must select at least one day and time");
+        return false;
+      }else{
+        EventsList.insert({
+          title: eventName,
+          name: userName,
+          dates: dates
+        });
+      }
+      
+    },
+    'click #addDate': function(event){
+      event.preventDefault();
+      var day = $("#my-datepicker").val();
+      var time = $('[name="time"]').val();
+      $("#my-datepicker").val("");
+      $('[name="time"]').val("");
+      if(thereIsAnEmptyString([day, time]))
+        alert('You must set a day and a time');
+      else
+        insertDate(day, time);
    },
   });
 
   Template.initForm.rendered=function() {
       $('#my-datepicker').datepicker({
-        format: "dd/mm/yyyy",
-        todayHighlight: true,
-        multidate: true
+        format: "dd/mm/yyyy"
       });
   }
 }
